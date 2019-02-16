728564e20810c2ad984254d7c6174842b7fb15e0
turtlemaster19 <46784000+turtlemaster19@users.noreply.github.com>
2019-02-15 20:02:44 -0600

    Add looping and self-hosted CORS Anywhere

--- siweather.js
+++ siweather.js
@@ -2,14 +2,18 @@
 Number.prototype.pad=function(size){let s=String(this);while(s.length<(size||2)){s="0"+s;}return s;};
 Number.prototype.sepThousands=function(sep){let p=String(this).split(".");p[0]=p[0].replace(/\B(?=(\d{3})+(?!\d))/g,sep);return p.join(".");};
 var getData=(url,callback)=>{let request=new XMLHttpRequest();request.addEventListener("load",()=>{request.responseText&&request.status<400?callback(request.responseText):console.log("Request failed: "+(request.responseText?request.responseText:"Error "+request.status));});request.open("GET",url);request.send()};
-var getMETAR=(SWbound, NEbound, date=[false,false,false], h=false, m=false)=>{
-	let url = "https://cors-anywhere.herokuapp.com/www.aviationweather.gov/cgi-bin/json/MetarJSON.php?bbox="+SWbound[0]+","+SWbound[1]+","+NEbound[0]+","+NEbound[1];
+var getMETAR=(loop, SWbound, NEbound, date=[false,false,false], h=false, m=false)=>{
+	let corsanywhere = "https://siweather.herokuapp.com/";
+	let url = corsanywhere+"www.aviationweather.gov/cgi-bin/json/MetarJSON.php?bbox="+SWbound[0]+","+SWbound[1]+","+NEbound[0]+","+NEbound[1];
 	url+=(date[0]?"&date="+date[0].pad(4)+date[1].pad(2)+date[2].pad(2)+h.pad(2)+m.pad(2):"");
 	getData(url, data=>{
 		let weather=JSON.parse(data).features[0];
 		if (!weather) {throw "Data Not Found";}
 		weather=weather.properties;
+		let rows = document.getElementById("weather").getElementsByTagName("tr").length;
 		let newRow=document.getElementById("weather").insertRow(-1),currentCell;
+		currentCell=newRow.insertCell(-1);
+		currentCell.appendChild(document.createTextNode(rows));
 		currentCell=newRow.insertCell(-1);
 		currentCell.appendChild(document.createTextNode((Date.parse(weather.obsTime)/1000).sepThousands(" ")+" s"));
 		currentCell=newRow.insertCell(-1);
@@ -19,7 +23,7 @@ var getMETAR=(SWbound, NEbound, date=[false,false,false], h=false, m=false)=>{
 		currentCell=newRow.insertCell(-1);
 		currentCell.appendChild(document.createTextNode((weather.wspd*463/900).toFixed(1)+" m\u22C5s\u207B\u00B9"));
 		currentCell=newRow.insertCell(-1);
-		currentCell.appendChild(document.createTextNode(weather.wdir+"\u00B0"));
+		currentCell.appendChild(document.createTextNode(weather.wdir!=360?weather.wdir+"\u00B0":"0\u00B0"));
 		currentCell=newRow.insertCell(-1);
 		currentCell.appendChild(document.createTextNode((weather.wgst?(weather.wgst*463/900).toFixed(1)+" m\u22C5s\u207B\u00B9":"None")));
 		currentCell=newRow.insertCell(-1);
@@ -36,10 +40,11 @@ var getMETAR=(SWbound, NEbound, date=[false,false,false], h=false, m=false)=>{
 		currentCell.appendChild(document.createTextNode((weather.altim/100).toFixed(3)+" kg\u22C5m\u207B\u00B9\u22C5s\u207B\u00B2"));
 		currentCell=newRow.insertCell(-1);
 		currentCell.appendChild(document.createTextNode(weather.slp?((weather.slp/100).toFixed(3)+" kg\u22C5m\u207B\u00B9\u22C5s\u207B\u00B2"):((weather.altim/100).toFixed(3)+" kg\u22C5m\u207B\u00B9\u22C5s\u207B\u00B2")));
+		if (loop){
 		let t=weather.obsTime;
-		let d=[parseInt(t.substring(0,4)),parseInt(t.substring(5,7)),parseInt(t.substring(8,10))],h=parseInt(t.substring(11,13)),m=t.substring(14,16)-1;
-		if(m<0){h--;m+=60;}
-		getMETAR(SWbound,NEbound,d,h,m);
+		let d=[parseInt(t.substring(0,4)),parseInt(t.substring(5,7)),parseInt(t.substring(8,10))],h=parseInt(t.substring(11,13));
+		getMETAR(true, SWbound,NEbound,d,h-1,59);
+		}
 	});
 };
-getMETAR([-98,32], [-97,33]);
+getMETAR(true, [-98,32], [-97,33]);
